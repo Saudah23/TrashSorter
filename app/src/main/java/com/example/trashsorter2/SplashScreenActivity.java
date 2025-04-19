11efde636542782bdc4af050b4c2c361cc2348a3
tiaramlgr <tiaraameliaanggraeni@gmail.com>
2025-04-19 22:10:04 +0800

    splashscreen

---
+++ app/src/main/java/com/example/trashsorter2/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package com.example.trashsorter2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Timer untuk splash screen
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // supaya tidak bisa kembali ke splash
+        }, SPLASH_DURATION);
+    }
+}
